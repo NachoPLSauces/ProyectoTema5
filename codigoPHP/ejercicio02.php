ba0c4d0fc4f88231563201b542206aad84229fa3
Nacho del Prado Losada <ignacio.pralos@educa.jcyl.es>
2020-11-30 13:44:16 +0100

    Modificado ejercicio02

--- codigoPHP/ejercicio02.php
+++ codigoPHP/ejercicio02.php
@@ -9,42 +9,41 @@
 require_once '../config/confDBPDO.php';
 
 //Si el usuario no se ha identificado se piden las creedenciales
-if (!isset($_SERVER['PHP_AUTH_USER'])) {
+if (!isset($_SERVER['PHP_AUTH_USER']) || !isset($_SERVER['PHP_AUTH_PW'])) {
     header('WWW-Authenticate: Basic Realm="Contenido restringido"');
     header('HTTP/1.0 401 Unauthorized');
-    echo "Usuario no reconocido!";
     exit;
 }
 //Una vez se han introducido se comprueban con la base de datos
-else{
+try{
     //Instanciar un objeto PDO y establecer la conexión con la base de datos
     $miDB = new PDO(DSN, USER, PASSWORD);
 
     //Establecer PDO::ERRMODE_EXCEPTION como valor del atributo PDO::ATTR_ERRMODE
     $miDB->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
     
-        //Almaceno la consulta a sql en una variable
-        $sql = "SELECT usuario FROM usuarios WHERE usuario={$_SERVER['PHP_AUTH_USER']} AND contrasena="
+    //Almaceno la consulta a sql en una variable
+    $sql = "SELECT CodUsuario, Password FROM usuarios WHERE CodUsuario=:CodUsuario";
+    //Preparo la consulta
+    $consulta = $miDB->prepare($sql);
+    $consulta->bindParam(":CodUsuario", $_SERVER['PHP_AUTH_USER']);
+    $consulta->execute();
     
-?>
-
-<!DOCTYPE html>
-<html>
-    <head>
-        <title>Ejercicios Tema 5</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    if($consulta->rowCount()>0){
+        //Se obtiene el primer registro
+        $registro = $consulta->fetchObject();
         
-    </head>
-    
-    <body>
-        <?php
-        echo 'Nombre de usuario: '.$_SERVER['PHP_AUTH_USER']."<br>";
-        echo 'Contraseña: '.$_SERVER['PHP_AUTH_PW']."<br>";
-        ?>        
-    </body>
-</html>
-
-<?php
+        //Se encripta el usuario junto con la contraseña y se almacena en una variable
+        $password = hash("sha256", $_SERVER['PHP_AUTH_USER'].$_SERVER['PHP_AUTH_PW']);
+        
+        if($registro->CodUsuario==$_SERVER['PHP_AUTH_USER'] && $registro->Password==$_SERVER['PHP_AUTH_PW']){
+            header("Location: programa.php");
+            exit;
+        }
+    }
+} catch (PDOException $pdoe){
+    echo "<p style='color: red'>ERROR: " . $pdoe->getMessage() . "</p>";
+} finally {
+    unset($miDB);
 }
 ?>
